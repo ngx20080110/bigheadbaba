016439694c79c1d50106f95b2773c1a43396b09a
Godwin.WU <wgy687@163.com>
2018-03-10 17:42:31 +0800

    add_kid layout finish

---
+++ app/src/main/java/com/ngx20080110/bigheadbaba/cowboy/AddKidActivity.java
@@ -0,0 +1,13 @@
+package com.ngx20080110.bigheadbaba.cowboy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddKidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_kid);
+    }
+}
